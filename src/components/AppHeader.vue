fa6fc16d2206a53316356d43f91c54f7302346e4
Marco-Guarnera <Darkaquon93@gmail.com>
2024-09-19 15:31:50 +0200

    Completed AppHeader.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<template>
+    <!-- Header -->
+    <header>
+        <h1>Yu-Gi-Oh</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    height: 75px;
+
+    h1 {
+        font-size: 2.5rem;
+        font-weight: bold;
+        line-height: 75px;
+        text-align: center;
+    }
+}
+</style>
